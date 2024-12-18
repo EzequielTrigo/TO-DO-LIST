040eb9278c3b4857c3c63b8e78c081de2ee5a21a
Ezequiel Trigo <ezequiel0trigo@gmail.com>
2024-12-18 14:13:33 -0300

    tuto3_1413

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("prueba/",views.prueba,name="prueba"),
+    path("<int:question_id>/",views.detail,name="details"),
+    path("<int:question_id>/results/",views.results,name="results"),
+    path("<int:question_id>/vote/",views.vote,name="vote"),
 ]
